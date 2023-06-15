379b1fcba499f6b791fd50d94fe2798bf3de67e1
Sherif Farouk <sherifmohamed2004@gmail.com>
2023-06-15 07:18:35 +0300

    other emails

--- lib/core/validation/validation.dart
+++ lib/core/validation/validation.dart
@@ -1,6 +1,6 @@
 extension ExtString on String {
 
-  // this validation for the @izam.co mails only
+  // this validation for the @izam.co email only
   bool get isValidIzamEmailOnly {
     final emailRegExp = RegExp(r'^[a-zA-Z]{3}.[0-9]{4}@izam.co$');
     return emailRegExp.hasMatch(this);
@@ -8,7 +8,6 @@ extension ExtString on String {
 
   // this validation for all the emails like @gmail.com and @yahoo.com
   bool get isValidEmailForEveryTypeOFEmail {
-    // final emailRegExp = RegExp(r'^[a-zA-Z]{3}.[0-9]{4}@izam.co$');
     final emailRegExp = RegExp(r'^[a-zA-Z]{3}.[0-9]{4}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
     return emailRegExp.hasMatch(this);
   }
